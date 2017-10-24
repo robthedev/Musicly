688015a8ed52053d3d5c9003d4d59bb26f0338fa
robthewebdev <robthewebdev@gmail.com>
2017-10-23 20:45:39 -0500

    update the customer controller with database context

---
+++ Musicly/Controllers/CustomersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Musicly.Models;
+
+namespace Musicly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //Create database context
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            //instatiate database context inside the contructor
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ViewResult Index()
+        {
+            //Deffered execution
+            var customers = _context.Customers;
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(cust => cust.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "User One"},
+                new Customer {Id = 2, Name = "User Two"}
+            };
+        }
+    }
+}
