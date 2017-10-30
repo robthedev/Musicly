9e83a23ad6628ae567bf3620e3571c6f18f80d4d
robthewebdev <robthewebdev@gmail.com>
2017-10-29 20:47:38 -0500

    changed view to display membership types, data migration to update membershiptype table with membership type names

--- Musicly/Migrations/201710300117249_UpdateMembershipTypeDatabaseWithNameInformation.cs
+++ Musicly/Migrations/201710300117249_UpdateMembershipTypeDatabaseWithNameInformation.cs
@@ -7,6 +7,10 @@ namespace Musicly.Migrations
     {
         public override void Up()
         {
+            Sql("UPDATE MembershipTypes SET Name = 'Pay as you go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 4");
         }
         
         public override void Down()
