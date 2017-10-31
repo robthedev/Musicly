2da4dbdd51bd7e7539530014715a8e07cad8b92e
robthewebdev <robthewebdev@gmail.com>
2017-10-30 20:02:41 -0500

    populated birth date for customer 1

--- Musicly/Migrations/201710310030089_PopulateCustomerBirthDate.cs
+++ Musicly/Migrations/201710310030089_PopulateCustomerBirthDate.cs
@@ -7,7 +7,7 @@ namespace Musicly.Migrations
     {
         public override void Up()
         {
-
+            Sql("UPDATE Customers SET BirthDate =  CAST('1980-01-01' AS DATETIME) WHERE Id = 1");
         }
         
         public override void Down()
