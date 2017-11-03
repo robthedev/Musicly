e9ed2ec00b389814b5eb767c5c9409880796491c
robthewebdev <robthewebdev@gmail.com>
2017-11-03 13:38:46 -0500

    updated song index and details view

--- Musicly/Controllers/SongsController.cs
+++ Musicly/Controllers/SongsController.cs
@@ -1,30 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Musicly.Models;
 
 namespace Musicly.Controllers
 {
     public class SongsController : Controller
     {
-        // GET: Songs
-        public ActionResult Index(int? pageIndex, string sortBy)
+        //Create database context
+        private ApplicationDbContext _context;
+
+        public SongsController()
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            //instatiate database context inside the contructor
+            _context = new ApplicationDbContext();
         }
 
-        // GET: Songs by release date
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+        protected override void Dispose(bool disposing)
         {
-            return Content(year + "/" + month);
+            _context.Dispose();
+        }
+
+        // GET: Songs
+        public ViewResult Index()
+        {
+            //Deffered execution
+            //eager loading -> brining over related model information (foreign key)
+            var songs = _context.Songs.Include(s => s.Genre).ToList();
+
+            return View(songs);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var song = _context.Songs.Include(s => s.Genre).SingleOrDefault(s => s.Id == id);
+
+            if (song == null)
+                return HttpNotFound();
+
+            return View(song);
         }
     }
 
