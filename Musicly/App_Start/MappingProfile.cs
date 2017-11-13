d96fe8554cea2a427f46d8f9001354ba5e4e8738
robthewebdev <robthewebdev@gmail.com>
2017-11-12 18:47:56 -0600

    added automapper lib, refactored customers controller to map props to customer dto

--- Musicly/App_Start/MappingProfile.cs
+++ Musicly/App_Start/MappingProfile.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AutoMapper;
+using Musicly.Models;
+using Musicly.DataTransferObjects;
 
 namespace Musicly.App_Start
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
+        //map object to object so you dont have to define the props manually (convention based mapping tool)
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
     }
 }
