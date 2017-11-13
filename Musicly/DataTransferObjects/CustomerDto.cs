37761193fb34011c152062ea5ed6dae6579a89d0
robthewebdev <robthewebdev@gmail.com>
2017-11-12 18:24:10 -0600

    created a directory for data transfer objects, created a dto for customer

---
+++ Musicly/DataTransferObjects/CustomerDto.cs
@@ -0,0 +1,30 @@
+﻿using Musicly.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Musicly.DataTransferObjects
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        //Data annotations
+        [Required(ErrorMessage = "Please enter name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        //nullable propperty
+        //to update teh label text
+        [AdultMember]
+        public DateTime? BirthDate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        //Foreign key
+        //byte makes this implictly required
+        public byte MembershipTypeId { get; set; }
+    }
+}
