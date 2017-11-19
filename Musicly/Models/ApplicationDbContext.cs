de2da6f8027750405adf32f8701fddb65b934959
robthewebdev <robthewebdev@gmail.com>
2017-11-19 14:07:42 -0600

    organized viewmodels, split into sep classes, split indetitymodels into sep classes

--- Musicly/Models/ApplicationDbContext.cs
+++ Musicly/Models/ApplicationDbContext.cs
@@ -11,22 +11,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Musicly.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        [Required]
-        [StringLength(255)]
-        public string PinCode { get; set; }
-
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
-
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
---
+++ Musicly/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Musicly.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        public string PinCode { get; set; }
+    }
+}
