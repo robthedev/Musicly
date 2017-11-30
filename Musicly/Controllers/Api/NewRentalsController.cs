c46cc47999104a7d13b62dfd57f3710c9f259a56
robthewebdev <robthewebdev@gmail.com>
2017-11-30 17:47:53 -0600

    added validation checks to rentals controller

--- Musicly/Controllers/Api/NewRentalsController.cs
+++ Musicly/Controllers/Api/NewRentalsController.cs
@@ -24,13 +24,32 @@ namespace Musicly.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
+            //for public api
+            //if (newRental.SongIds.Count == 0)
+                //return BadRequest("No songs rented");
+
             //single over singleordefualt for internal purposes, customer being picked from data list
             var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
-            var songs = _context.Songs.Where(s => newRental.SongIds.Contains(s.Id));
+
+            //for public api
+            //var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+             //if (customer == null)
+               // return BadRequest("CustomerId in not valid");
+
+            var songs = _context.Songs.Where(s => newRental.SongIds.Contains(s.Id)).ToList();
+
+            //for public api
+            //if (songs.Count != newRental.SongIds.Count)
+                //return BadRequest("One or More of the songs are invlaid");
 
             //each rented song creates a new rental object
             foreach (var song in songs)
             {
+                if (song.NumberAvailable == 0)
+                    return BadRequest("Song is not available");
+
+                song.NumberAvailable--;
+
                 var rental = new Rental
                 {
                     Customer = customer,
