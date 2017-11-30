bbec8eb835d0b1208d4e17a04846f3d5c47fd623
robthewebdev <robthewebdev@gmail.com>
2017-11-30 16:12:10 -0600

    created api action to rent song

--- Musicly/Controllers/Api/NewRentalsController.cs
+++ Musicly/Controllers/Api/NewRentalsController.cs
@@ -5,15 +5,45 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Musicly.DataTransferObjects;
+using Musicly.Models;
 
 namespace Musicly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        //create a db context
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            //initialize the db context within the constructor
+            _context = new ApplicationDbContext();
+        }
+
+        //api/newrentals
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            //single over singleordefualt for internal purposes, customer being picked from data list
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            var songs = _context.Songs.Where(s => newRental.SongIds.Contains(s.Id));
+
+            //each rented song creates a new rental object
+            foreach (var song in songs)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Song = song,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
